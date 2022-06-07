da1af9f58d6abd28dc735c4e5e28e7a3b84f7ab5
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-06-06 20:44:57 -0500

    commit tree 582600c2f8e66fab62192faf77aad37fe2a5b783

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+{
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+}
+
+let random = 5
+Math.random()
+
+{
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4
+}
+
+{
+    let max = Math.max(1, 2, 3, 4, 20);
+}
+
+
 
 // Write your code here
